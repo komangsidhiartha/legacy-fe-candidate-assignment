12189fe728ee01efdd16cd502df3927609aafa34
Komang Sidhi Artha <sidhi.artha@gmail.com>
2025-09-05 10:07:52 +0700

    feat: comply with render deployment method

--- backend-rehearshal/src/app.ts
+++ backend-rehearshal/src/app.ts
@@ -1,28 +1,28 @@
 import express = require('express');
 import cors = require('cors');
-import { ethers } from 'ethers';
+import {ethers} from 'ethers';
 
 const app = express();
-const port = 5001;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/verify-signature', (req, res) => {
-    const { message, signature } = req.body;
-    try {
-        const signer = ethers.verifyMessage(message, signature);
-        const isValid = !!signer;
-        res.json({ isValid, signer, originalMessage: message });
-    } catch (error) {
-        res.status(400).json({ isValid: false, signer: null, originalMessage: message });
-    }
+  const {message, signature} = req.body;
+  try {
+    const signer = ethers.verifyMessage(message, signature);
+    const isValid = !!signer;
+    res.json({isValid, signer, originalMessage: message});
+  } catch (error) {
+    res.status(400).json({isValid: false, signer: null, originalMessage: message});
+  }
 });
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World');
 // });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
